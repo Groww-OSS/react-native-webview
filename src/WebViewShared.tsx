e858588be188231e894c88d85fb666b50d7891d8
Matias Lang <matias.la@exodus.io>
2023-03-17 18:37:23 -0300

    Revert "don't use react-native's Linking"
    It makes sense to open URLs not matching the origin whitelist in a
    regular browser. However, we need to make sure we only support
    HTTP/HTTPS schemes. We don't want deep links or any other weird URIs to
    be opened.

--- src/WebViewShared.tsx
+++ src/WebViewShared.tsx
@@ -1,6 +1,6 @@
 import escapeStringRegexp from 'escape-string-regexp';
 import React, { useCallback, useMemo, useRef, useState } from 'react';
-import { View, ActivityIndicator, Text, Platform } from 'react-native';
+import { Linking, View, ActivityIndicator, Text, Platform } from 'react-native';
 import {
   OnShouldStartLoadWithRequest,
   ShouldStartLoadRequestEvent,
@@ -55,6 +55,15 @@ const createOnShouldStartLoadWithRequest = (
     const { url, lockIdentifier } = nativeEvent;
 
     if (!_passesWhitelist(compileWhitelist(originWhitelist), url)) {
+      Linking.canOpenURL(url).then((supported) => {
+        if (supported && /^https:\/\//.test(url)) {
+          return Linking.openURL(url);
+        }
+        console.warn(`Can't open url: ${url}`);
+        return undefined;
+      }).catch(e => {
+        console.warn('Error opening URL: ', e);
+      });
       shouldStart = false;
     } else if (onShouldStartLoadWithRequest) {
       shouldStart = onShouldStartLoadWithRequest(nativeEvent);
